cf333711285002367d56f2ed81a4bd55efbf6143
Matheus Luís Webber <matheus.webber@hotmail.com>
2022-09-01 17:52:14 -0300

    Fiz algumas melhorias nas funções de unsharp e laplaciano e também adicionei funções para a detecção de bordas usando prewitt e sobel

--- questao_1.py
+++ questao_1.py
@@ -4,122 +4,178 @@ from PIL import Image
 
 def image_to_matrix(image: Image) -> list:
     image_matrix = []
-    for i in range(image.height):
-        image_matrix.append([image.getpixel((0, i))])
-        for j in range(1, image.width):
-            image_matrix[i].append(image.getpixel((j, i)))
+    for line in range(image.height):
+        image_matrix.append([image.getpixel((0, line))])
+        for column in range(1, image.width):
+            image_matrix[line].append(image.getpixel((line, column)))
     return image_matrix
 
 
 def matrix_to_image(matrix: list, image: Image) -> Image:
     image = image.copy()
-    for x in range(len(matrix)):
-        for y in range(len(matrix[x])):
-            image.putpixel((x, y), matrix[x][y])
+    for line in range(len(matrix)):
+        for column in range(len(matrix[line])):
+            image.putpixel((line, column), matrix[line][column])
     return image
 
 
-def create_empty_matrix(n, m) -> list:
+def create_empty_matrix(m, n) -> list:
     image_matrix = []
-    for i in range(m):
+    for line in range(m):
         image_matrix.append([])
-        for j in range(n):
-            image_matrix[i].append(0)
+        for column in range(n):
+            image_matrix[line].append(0)
     return image_matrix
 
 
-def apply_filter(image: Image, mask: list, interest_pixel: tuple) -> list:
-    pass
+def create_empty_mask(m: int, n: int, default_value: int = 1) -> list:
+    mask = []
+    for line in range(m):
+        mask.append([])
+        for column in range(n):
+            mask[line].append(default_value)
+    return mask
 
 
-def laplacian(image: Image, mask_type: string, modify: bool = False) -> Image:
-    if not modify:
-        image = image.copy()
-    laplacian_mask = []
-    if mask_type == 'cruz':
-        for i in range(3):
-            laplacian_mask.append([0] * 3)
-        laplacian_mask[0][1], laplacian_mask[1][0], laplacian_mask[2][1], laplacian_mask[1][2] = [1, 1, 1, 1]
-        laplacian_mask[1][1] = -4
-    elif mask_type == 'diagonal':
-        for i in range(3):
-            laplacian_mask.append([0] * 3)
-        for i in range(3):
-            for j in range(3):
-                if j == 0 and i == 0:
-                    laplacian_mask[i][j] = -8
-                else:
-                    laplacian_mask[i][j] = 1
-
-    image_matrix = image_to_matrix(image)
-    for y in range(1, image.height - 1):
-        for x in range(1, image.width - 1):
-            central_pixel = 0
-            for y1 in range(-1, 2):
-                for x1 in range(-1, 2):
-                    central_pixel += laplacian_mask[x1][y1] * image_matrix[x-x1][y-y1]
-            image.putpixel((y, x), central_pixel)
-            # if central_pixel < 0:
-            #     central_pixel *= -1
-            #     if central_pixel % 2 == 0:
-            #         resultado = int(central_pixel / 2)
-            #         image.putpixel((y, x), resultado)
-            #     else:
-            #         resultado = int((central_pixel - 1) / 2)
-            #         image.putpixel((y, x), resultado)
-            # else:
-            #     if central_pixel % 2 == 0:
-            #         resultado = int(central_pixel / 2) + 127
-            #         image.putpixel((y, x), resultado)
-            #     else:
-            #         resultado = int((central_pixel - 1) / 2) + 127
-            #         image.putpixel((y, x), resultado)
-    return image
-
-
-def unsharp_masking(image: Image, n: int, m: int, k: float, modify: bool = False) -> Image:
+def arithmetic_mean_filter(image: Image, m: int, n: int, modify: bool = False) -> Image:
     if not modify:
         image = image.copy()
     original_image_matrix = image_to_matrix(image)
     blurred_image_matrix = create_empty_matrix(image.height, image.width)
-    n1 = (n/2).__floor__()
     m1 = (m/2).__floor__()
-    for y in range(image.height):
-        for x in range(image.width):
+    n1 = (n/2).__floor__()
+    for line in range(image.height):
+        for column in range(image.width):
             central_pixel = 0
-            for y1 in range(-n1, n1+1):
-                for x1 in range(-m1, m1+1):
+            for mask_line in range(-m1, m1+1):
+                for mask_column in range(-n1, n1+1):
                     try:
-                        central_pixel += original_image_matrix[x+x1][y+y1]
+                        central_pixel += original_image_matrix[line+mask_line][column+mask_column]
                     except IndexError:
                         pass
-            blurred_image_matrix[y][x] = int(central_pixel/(m*n))
+            blurred_image_matrix[line][column] = int(central_pixel/(m*n))
+    return matrix_to_image(blurred_image_matrix, image)
 
-    for y in range(image.height):
-        for x in range(image.width):
-            image.putpixel((x, y), original_image_matrix[y][x] + int(k * original_image_matrix[y][x] -
-                                                                     blurred_image_matrix[x][y]))
+
+def apply_linear_filter(image: Image, mask: list) -> list:
+    original_image_matrix = image_to_matrix(image)
+    filtered_image_matrix = create_empty_matrix(image.height, image.width)
+    m1 = (len(mask)/2).__floor__()
+    n1 = (len(mask[0])/2).__floor__()
+    for line in range(image.height):
+        for column in range(image.width):
+            central_pixel = 0
+            for mask_line in range(-m1, m1+1):
+                for mask_column in range(-n1, n1+1):
+                    try:
+                        central_pixel += original_image_matrix[line+mask_line][column+mask_column] *\
+                                         mask[mask_line+m1][mask_column+n1]
+                    except IndexError:
+                        pass
+            filtered_image_matrix[line][column] = central_pixel
+    return filtered_image_matrix
+
+
+def laplacian_filter(image: Image, center_value: int = -8, modify: bool = False, adjusted_laplacian: bool = False) -> Image:
+    if not modify:
+        image = image.copy()
+    if center_value == -8:
+        laplacian_mask = [[1, 1, 1], [1, -8, 1], [1, 1, 1]]
+    elif center_value == 8:
+        laplacian_mask = [[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]]
+    elif center_value == -4:
+        laplacian_mask = [[0, 1, 0], [1, -4, 1], [0, 1, 0]]
+    elif center_value == 4:
+        laplacian_mask = [[0, -1, 0], [-1, 4, -1], [0, -1, 0]]
+
+    laplacian_image = image.copy()
+    laplacian_image_matrix = apply_linear_filter(image, laplacian_mask)
+    if adjusted_laplacian:
+        for line in range(len(laplacian_image_matrix)):
+            for column in range(len(laplacian_image_matrix[line])):
+                pixel = laplacian_image_matrix[line][column]
+                if pixel == 0:
+                    laplacian_image.putpixel((line, column), 127)
+                elif pixel < 0:
+                    pixel *= -1
+                    if pixel % 2 == 0:
+                        laplacian_image.putpixel((line, column), 127 - int(pixel / 2))
+                    else:
+                        laplacian_image.putpixel((line, column), 127 - int((pixel - 1) / 2))
+                else:
+                    if pixel % 2 == 0:
+                        laplacian_image.putpixel((line, column), int(pixel / 2) + 127)
+                    else:
+                        laplacian_image.putpixel((line, column), int((pixel - 1) / 2) + 127)
+    else:
+        laplacian_image = matrix_to_image(laplacian_image_matrix, image)
+    for line in range(image.height):
+        for column in range(image.width):
+            if center_value < 0:
+                image.putpixel((line, column), image.getpixel((line, column)) - laplacian_image_matrix[line][column])
+            else:
+                image.putpixel((line, column), image.getpixel((line, column)) + laplacian_image_matrix[line][column])
+
+    return image, laplacian_image
+
+
+def unsharp_masking(image: Image, n: int, m: int, modify: bool = False) -> Image:
+    if not modify:
+        image = image.copy()
+    original_image_matrix = image_to_matrix(image)
+    blurred_image = arithmetic_mean_filter(image, n, m)
+    blurred_image_matrix = image_to_matrix(blurred_image)
+
+    for line in range(image.height):
+        for column in range(image.width):
+            image.putpixel((line, column), original_image_matrix[line][column] + 
+            (original_image_matrix[line][column] - blurred_image_matrix[line][column]))
     return image
 
 
-def highboost():
-    pass
+def high_boost(image: Image, m: int, n: int, k: float, modify: bool = False) -> Image:
+    if not modify:
+        image = image.copy()
+    original_image_matrix = image_to_matrix(image)
+    blurred_image = arithmetic_mean_filter(image, n, m)
+    blurred_image_matrix = image_to_matrix(blurred_image)
+
+    for line in range(image.height):
+        for column in range(image.width):
+            image.putpixel((line, column), original_image_matrix[line][column] + int(k * 
+            (original_image_matrix[line][column] - blurred_image_matrix[line][column])))
+    return image
 
 
-def prewitt_border_detection():
-    pass
+def prewitt_border_detection(image: Image) -> Image:
+    prewitt_mask = [[-1, -1, -1], [0, 0, 0], [1, 1, 1]]
+    prewitt_image = apply_linear_filter(image, prewitt_mask)
+    return matrix_to_image(prewitt_image, image)
 
 
-def sobel_border_detection():
-    pass
+
+def sobel_border_detection(image: Image):
+    sobel_mask = [[-1, -2, -1], [0, 0, 0], [1, 2, 1]]
+    sobel_image = apply_linear_filter(image, sobel_mask)
+    return matrix_to_image(sobel_image, image)
 
 
 if __name__ == '__main__':
-    # with Image.open('imgs/lena_gray.bmp', 'r') as lena_gray:
-        # result = laplacian(lena_gray, 'diagonal')
-        # result.show()
-        # result.save('results/lena_diag.bmp')
     with Image.open('imgs/lena_gray.bmp', 'r') as lena_gray:
-        result = unsharp_masking(lena_gray, 5, 5, 1)
-        result.save('results/lena_gray_sharpen.bmp')
+        result, laplacian_image = laplacian_filter(lena_gray, 4, adjusted_laplacian=True)
+        laplacian_image.show()
+        laplacian_image.save('imgs/lena_clack_white_laplacian.bmp')
         result.show()
+        result.save('results/lena_laplacian.bmp')
+
+        # result = high_boost(lena_gray, 5, 5, 5)
+        # result.save('results/lena_gray_unsharp.bmp')
+        # result.show()
+
+        # result = prewitt_border_detection(lena_gray)
+        # result.save('results/lena_prewitt.bmp')
+        # # result.show()
+
+        # result = sobel_border_detection(lena_gray)
+        # result.save('results/lena_sobel.bmp')
+        # result.show()
